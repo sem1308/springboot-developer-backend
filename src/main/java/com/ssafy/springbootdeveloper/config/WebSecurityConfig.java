ed907490210077a883bfdf5869181b87957b1ed1
한수한 <hshhan1009@naver.com>
2024-05-06 22:53:50 +0900

    feat : spring security 적용

---
+++ src/main/java/com/ssafy/springbootdeveloper/config/WebSecurityConfig.java
@@ -0,0 +1,71 @@
+package com.ssafy.springbootdeveloper.config;
+
+import com.ssafy.springbootdeveloper.user.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Collections;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebSecurityConfig {
+    // 스프링 시큐리티 기능 비활성화
+    // 정적 리소스만 스프링 시큐리티 사용을 비활성화
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return (web) -> web.ignoring()
+            .requestMatchers(toH2Console())
+            .requestMatchers("/static/**");
+    }
+
+    // 특정 HTTP 요청에 대한 웹 기반 보안 구성
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+            .authorizeHttpRequests() // 인증, 인가 설정
+            .requestMatchers("/api/articles/**", "/api/users/**").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .csrf().disable() // csrf 비활성화
+            .cors(corsCustomizer -> corsCustomizer.configurationSource((request) ->{
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+                    config.setAllowedMethods(Collections.singletonList("*"));
+                    config.setAllowCredentials(true);
+                    config.setAllowedHeaders(Collections.singletonList("*"));
+                    config.setMaxAge(3600L); //1시간
+                    return config;
+            }))
+            .build();
+    }
+
+    // 인증 관리자 관련 설정
+    @Bean
+    public AuthenticationManager authenticationManager(
+        HttpSecurity http,
+        BCryptPasswordEncoder bCryptPasswordEncoder,
+        UserService userService
+    ) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+            .userDetailsService(userService)
+            .passwordEncoder(bCryptPasswordEncoder)
+            .and()
+            .build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
