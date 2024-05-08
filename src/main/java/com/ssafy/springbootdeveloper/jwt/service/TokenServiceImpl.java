a600c03484aadd425595934c423efd6d426e226c
sem1308 <43841214+sem1308@users.noreply.github.com>
2024-05-08 18:00:14 +0900

    feat : 로그인 구현 (토큰 발급)

--- src/main/java/com/ssafy/springbootdeveloper/jwt/service/TokenServiceImpl.java
+++ src/main/java/com/ssafy/springbootdeveloper/jwt/service/TokenServiceImpl.java
@@ -1,22 +1,21 @@
 package com.ssafy.springbootdeveloper.jwt.service;
 
+import com.ssafy.springbootdeveloper.config.jwt.JwtProperties;
 import com.ssafy.springbootdeveloper.config.jwt.TokenProvider;
+import com.ssafy.springbootdeveloper.jwt.domain.RefreshToken;
+import com.ssafy.springbootdeveloper.jwt.repository.RefreshTokenRespository;
 import com.ssafy.springbootdeveloper.user.domain.User;
 import com.ssafy.springbootdeveloper.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.time.Duration;
 
 @RequiredArgsConstructor
 @Service
 public class TokenServiceImpl implements TokenService {
     private final TokenProvider tokenProvider;
     private final RefreshTokenService refreshTokenService;
+    private final RefreshTokenRespository refreshTokenRespository;
     private final UserService userService;
-
-    private final static Duration ACCESS_TOKEN_DURATION = Duration.ofHours(2);
-    private final static Duration REFRESH_TOKEN_DURATION = Duration.ofHours(10);
 
     @Override
     public String createNewAccessToken(String refreshToken) {
@@ -33,11 +32,13 @@ public class TokenServiceImpl implements TokenService {
 
     @Override
     public String createAccessToken(User user) {
-        return tokenProvider.generateToken(user, ACCESS_TOKEN_DURATION);
+        return tokenProvider.generateToken(user, JwtProperties.ACCESS_TOKEN_DURATION);
     }
 
     @Override
     public String createRefreshToken(User user) {
-        return tokenProvider.generateToken(user, REFRESH_TOKEN_DURATION);
+        String refreshToken = tokenProvider.generateToken(user, JwtProperties.REFRESH_TOKEN_DURATION);
+        refreshTokenRespository.save(new RefreshToken(user.getId(), refreshToken));
+        return refreshToken;
     }
 }
--- src/main/java/com/ssafy/springbootdeveloper/user/controller/UserApiController.java
+++ src/main/java/com/ssafy/springbootdeveloper/user/controller/UserApiController.java
@@ -1,11 +1,15 @@
 package com.ssafy.springbootdeveloper.user.controller;
 
+import com.ssafy.springbootdeveloper.config.jwt.TokenProvider;
+import com.ssafy.springbootdeveloper.jwt.service.TokenService;
+import com.ssafy.springbootdeveloper.user.domain.User;
 import com.ssafy.springbootdeveloper.user.dto.AddUserRequest;
 import com.ssafy.springbootdeveloper.user.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.*;
@@ -20,5 +24,12 @@ public class UserApiController {
     public ResponseEntity<Long> signUp(@RequestBody AddUserRequest request){
         long id = userService.save(request);
         return ResponseEntity.ok(id);
+    }
+
+    @GetMapping
+    public ResponseEntity<User> getUser(Authentication authentication){
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User)authentication.getPrincipal();
+        return ResponseEntity.ok(user);
     }
 }
