c058dea99045af7cb6af171430168166d9c17506
sem1308 <43841214+sem1308@users.noreply.github.com>
2024-05-08 17:59:12 +0900

    Update JwtProperties.java

--- src/main/java/com/ssafy/springbootdeveloper/config/jwt/JwtProperties.java
+++ src/main/java/com/ssafy/springbootdeveloper/config/jwt/JwtProperties.java
@@ -5,6 +5,8 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
+
 @Setter
 @Getter
 @Component
@@ -12,4 +14,6 @@ import org.springframework.stereotype.Component;
 public class JwtProperties {
     private String issuer;
     private String secretKey;
+    public final static Duration ACCESS_TOKEN_DURATION = Duration.ofHours(1);
+    public final static Duration REFRESH_TOKEN_DURATION = Duration.ofHours(6);
 }
