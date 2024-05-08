87915a1d4a69eb961946c7990d4b8f64bc21135f
sem1308 <43841214+sem1308@users.noreply.github.com>
2024-05-08 17:55:39 +0900

    docs : 스프링 시큐리티 적용 전 cors config에 deprecated 설정

--- src/main/java/com/ssafy/springbootdeveloper/config/CorsConfig.java
+++ src/main/java/com/ssafy/springbootdeveloper/config/CorsConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Deprecated(since = "spring security 사용으로 인한 설정 파일 변경 -> WebSecurityConfig")
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
