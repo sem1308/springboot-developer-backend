e0bcfea562b77993e57b68c753c64a83aa0a9a94
sem1308 <43841214+sem1308@users.noreply.github.com>
2024-05-08 17:56:06 +0900

    feat : jwt filter 등록

--- src/main/java/com/ssafy/springbootdeveloper/config/WebSecurityConfig.java
+++ src/main/java/com/ssafy/springbootdeveloper/config/WebSecurityConfig.java
@@ -1,16 +1,21 @@
 package com.ssafy.springbootdeveloper.config;
 
+import com.ssafy.springbootdeveloper.config.jwt.TokenProvider;
+import com.ssafy.springbootdeveloper.filter.TokenAuthenticationFilter;
 import com.ssafy.springbootdeveloper.user.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.Collections;
@@ -21,20 +26,20 @@ import static org.springframework.boot.autoconfigure.security.servlet.PathReques
 @Configuration
 public class WebSecurityConfig {
     // 스프링 시큐리티 기능 비활성화
-    // 정적 리소스만 스프링 시큐리티 사용을 비활성화
+    // h2 console만 시큐리티 기능 사용을 비활성화
     @Bean
     public WebSecurityCustomizer configure() {
         return (web) -> web.ignoring()
-            .requestMatchers(toH2Console())
-            .requestMatchers("/static/**");
+            .requestMatchers(toH2Console());
     }
 
     // 특정 HTTP 요청에 대한 웹 기반 보안 구성
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, TokenProvider tokenProvider) throws Exception {
         return http
             .authorizeHttpRequests() // 인증, 인가 설정
-            .requestMatchers("/api/articles/**","/api/token/login", "/api/users/**").permitAll()
+            .requestMatchers("/api/articles/**","/api/token/login").permitAll()
+            .requestMatchers(HttpMethod.POST,"/api/users").permitAll()
             .anyRequest().authenticated()
             .and()
             .csrf().disable() // csrf 비활성화
@@ -47,6 +52,7 @@ public class WebSecurityConfig {
                     config.setMaxAge(3600L); //1시간
                     return config;
             }))
+            .addFilterBefore(new TokenAuthenticationFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class)
             .build();
     }
 
