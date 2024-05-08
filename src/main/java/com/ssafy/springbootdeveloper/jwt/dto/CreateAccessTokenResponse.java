6fbbaf6f388ea8439743051cf2802dc9ed02ca48
sem1308 <43841214+sem1308@users.noreply.github.com>
2024-05-08 17:59:41 +0900

    Update CreateAccessTokenResponse.java

--- src/main/java/com/ssafy/springbootdeveloper/jwt/dto/CreateAccessTokenResponse.java
+++ src/main/java/com/ssafy/springbootdeveloper/jwt/dto/CreateAccessTokenResponse.java
@@ -2,8 +2,10 @@ package com.ssafy.springbootdeveloper.jwt.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class CreateAccessTokenResponse {
     private String accessToken;
