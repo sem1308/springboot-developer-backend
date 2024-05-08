f1d7e59a400d7e2e2b53dfcb5718b98d8e7c1b68
sem1308 <43841214+sem1308@users.noreply.github.com>
2024-05-08 17:59:44 +0900

    Update CreateTokenRequest.java

--- src/main/java/com/ssafy/springbootdeveloper/jwt/dto/CreateTokenRequest.java
+++ src/main/java/com/ssafy/springbootdeveloper/jwt/dto/CreateTokenRequest.java
@@ -2,10 +2,13 @@ package com.ssafy.springbootdeveloper.jwt.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateTokenRequest {
     private String email;
     private String password;
