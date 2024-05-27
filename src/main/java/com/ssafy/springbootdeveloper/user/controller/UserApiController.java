9d933461004d16f40b0c118b3c830d9ed6a23979
한수한 <hshhan0221@naver.com>
2024-05-27 20:52:38 +0900

    fix : User 반환하지 않고 User Dto 반환하도록 변경

--- src/main/java/com/ssafy/springbootdeveloper/user/controller/UserApiController.java
+++ src/main/java/com/ssafy/springbootdeveloper/user/controller/UserApiController.java
@@ -2,8 +2,10 @@ package com.ssafy.springbootdeveloper.user.controller;
 
 import com.ssafy.springbootdeveloper.user.domain.User;
 import com.ssafy.springbootdeveloper.user.dto.AddUserRequest;
+import com.ssafy.springbootdeveloper.user.dto.GetUserResponse;
 import com.ssafy.springbootdeveloper.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -17,13 +19,18 @@ public class UserApiController {
     @PostMapping
     public ResponseEntity<Long> signUp(@RequestBody AddUserRequest request){
         long id = userService.save(request);
-        return ResponseEntity.ok(id);
+        return ResponseEntity.status(HttpStatus.CREATED).body(id);
     }
 
     @GetMapping
-    public ResponseEntity<User> getUser(Authentication authentication){
+    public ResponseEntity<GetUserResponse> getUser(Authentication authentication){
 //        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        User user = (User)authentication.getPrincipal();
-        return ResponseEntity.ok(user);
+        if(authentication.getPrincipal() instanceof User u){
+            User user = userService.findByEmail(u.getEmail());
+            GetUserResponse userResponse = new GetUserResponse(user);
+            return ResponseEntity.ok(userResponse);
+        }else{
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
