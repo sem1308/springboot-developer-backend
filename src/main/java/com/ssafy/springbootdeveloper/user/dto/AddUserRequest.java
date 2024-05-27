81e5b2a56d28071d56aa4e710936d400371a3e46
한수한 <hshhan0221@naver.com>
2024-05-27 20:51:00 +0900

    fix : request에 builder 추가

--- src/main/java/com/ssafy/springbootdeveloper/user/dto/AddUserRequest.java
+++ src/main/java/com/ssafy/springbootdeveloper/user/dto/AddUserRequest.java
@@ -1,12 +1,14 @@
 package com.ssafy.springbootdeveloper.user.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@Builder
 public class AddUserRequest {
     private String email;
     private String password;
