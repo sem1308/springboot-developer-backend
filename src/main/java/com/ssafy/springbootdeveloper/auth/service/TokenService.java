1200f3b9a500db4fde81ddf83fb5e17908d29b48
한수한 <hshhan0221@naver.com>
2024-06-07 22:54:35 +0900

    Update TokenService.java

--- src/main/java/com/ssafy/springbootdeveloper/auth/service/TokenService.java
+++ src/main/java/com/ssafy/springbootdeveloper/auth/service/TokenService.java
@@ -1,9 +1,14 @@
 package com.ssafy.springbootdeveloper.auth.service;
 
 import com.ssafy.springbootdeveloper.user.domain.User;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public interface TokenService {
     String createNewAccessToken(String refreshToken);
     String createAccessToken(User user);
     String createRefreshToken(User user);
+
+    void addRefreshTokenToCookie(HttpServletRequest request,
+                                 HttpServletResponse response, String refreshToken);
 }
